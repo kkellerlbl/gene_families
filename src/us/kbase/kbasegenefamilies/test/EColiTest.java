ca08e49d3aec94e6a5cd1a3b06140e200331614e
John-Marc Chandonia <JMChandonia@lbl.gov>
2014-12-18 19:32:05 +0000

    Started parsing CDD libraries

--- src/us/kbase/kbasegenefamilies/test/EColiTest.java
+++ src/us/kbase/kbasegenefamilies/test/EColiTest.java
@@ -4,17 +4,17 @@ import java.io.*;
 import java.util.*;
 import java.net.URL;
 
+import org.junit.Test;
 import static junit.framework.Assert.*;
 
-import org.junit.Test;
-
+import org.strbio.IO;
 import us.kbase.workspace.*;
 import us.kbase.kbasegenomes.*;
 import us.kbase.kbasegenefamilies.*;
 import com.fasterxml.jackson.databind.*;
 
 /**
-   Annotates E. coli
+   Tests for setting up sample db and annotating E. coli
 */
 public class EColiTest {
     // private static final String wsUrl = "http://dev04.berkeley.kbase.us:7058";
@@ -22,6 +22,10 @@ public class EColiTest {
     private static final String wsName = "KBasePublicGenomesV4";
     private static final String ecoliRef = wsName+"/kb|g.0";
 
+    /**
+       check that we can read E coli genome from WS or file;
+       if we get from WS, cache in file to avoid delays/loading server
+    */
     @Test public void getEColi() throws Exception {
 	Genome genome = null;
 	
@@ -49,6 +53,42 @@ public class EColiTest {
 	System.out.println(genome.getScientificName());
 	assertEquals(genome.getScientificName(), "Escherichia coli K12");
     }
+
+    /**
+       Make a DomainModelSet for COGs
+    */
+    @Test public void getCOGs() throws Exception {
+	ObjectMapper mapper = new ObjectMapper();
+
+	BufferedReader infile = IO.openReader("/kb/dev_container/modules/gene_families/data/db/cddid.tbl.gz");
+	String buffer;
+	while ((buffer=infile.readLine()) != null) {
+	    StringTokenizer st = new StringTokenizer(buffer,"\t");
+	    
+	}
+
+	DomainModel m = new DomainModel();
+
+	DomainLibrary dl = new DomainLibrary()
+	    .withId("COGs-CDD-3.12")
+	    .withSource("CDD")
+	    .withSourceUrl("ftp://ftp.ncbi.nih.gov/pub/mmdb/cdd/")
+	    .withVersion("3.12")
+	    .withReleaseDate("2014-10-03")
+	    .withProgram("rpsblast-2.2.30")
+	    .withDomainPrefix("COG")
+	    .withDbxrefPrefix("http://www.ncbi.nlm.nih.gov/Structure/cdd/cddsrv.cgi?uid=")
+	    .withLibraryFiles(null);
+
+	DomainModelSet dms = new DomainModelSet().withSetName("COGs-only set");
+    }
+
+
+    @Test public void searchEColi() throws Exception {
+	DomainSearchTask dst = new DomainSearchTask(new File("/kb/dev_container/modules/gene_families/data/tmp"), null);
+	// Tuple2<DomainAnnotation, DomainAlignments> results = dst.runDomainSearch(genome,ecoliRef,"/kb/dev_container/modules/gene_families/data/db/Cog.rps",nulll);
+    }
+    
 
     public static void main(String[] args) {
 	try {
